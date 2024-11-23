e23eea62e47aef413b271841758276984a7cbbdb
Timo Mämecke <timo@maemecke.com>
2024-11-23 22:03:31 +0100

    Align with reproduction-template

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     dynamicIO: true
-  }
+  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
